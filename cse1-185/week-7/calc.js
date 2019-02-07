a13e9815180a4d58dcea5cec7c04ad7a53ecaf64
diru1100 <rohitdandamudi.1100@gmail.com>
2019-02-07 12:35:33 +0530

    Completed calaculator program

--- cse1-185/week-7/calc.js
+++ cse1-185/week-7/calc.js
@@ -1,31 +1,11 @@
-
 function keepvalue(b){
- document.getElementById("output").value +=b;
-}
-
-function basicop(value){
-  var expp =value;
-  var sexp =expp.split("");
-  var sign=sexp[1];
-  var num1 = parseInt(sexp[0]);
-  var num2 = parseInt(sexp[2]);
-  sum=num1+num2;
-  diff=num1-num2;
-  prod=num1*num2;
-  div=num1/num2;
-  switch(sign){
-    case +:
-      document.(getElementById("output").value= sum.toString());
-      break;
-    case -:
-      document.(getElementById("output").value= diff.toString());
-      break;
-    case *:
-      document.(getElementById("output").value= prod.toString());
-      break;     
-    default:
-      document.(getElementById("output").value= div.toString());
-      break;       
-  }
+ document.getElementById('output').value+=b;
 
 }
+
+function evaluate1(){
+  var result = document.getElementById('output').value ;
+  document.getElementById('output').value=eval( result);
+
+}
+
